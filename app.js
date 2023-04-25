37ddc30a0af7045ff7222e209213892e7a7c37a8
JingWang <wangjing061993@gmail.com>
2023-04-25 19:28:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const width = document.getElementById('my-canvas').width = screen.availWidth;
+
 const height = document.getElementById('my-canvas').height = screen.availHeight;
 
 const ctx = document.getElementById('my-canvas').getContext('2d');
 
-const arr = Array(Math.ceil(width/10)).fill(0);
+const arr = Array(Math.ceil(width / 10)).fill(0);
 
 const str =
-"☇☈ ↼ ↽ ↾ ↿ ⇀ ⇁ ⇂ ⇃ ⇞ ⇟ ⇠ ⇡ ⇢ ⇣ ⇤ ⇥ ⇦ ⇧ ⇨ ⇩ ⇪ ↺ ↻" +
-"↑ ↓ ← → ↖ ↗ ↙ ↘ ↔ ↕ ➻ ➼ ➽ ➸ ➳ ➺ ➻ ➴ ➵ ➶ ➷ ➹".split("");
+    "☇☈ ↼ ↽ ↾ ↿ ⇀ ⇁ ⇂ ⇃ ⇞ ⇟ ⇠ ⇡ ⇢ ⇣ ⇤ ⇥ ⇦ ⇧ ⇨ ⇩ ⇪ ↺ ↻" +
+    "↑ ↓ ← → ↖ ↗ ↙ ↘ ↔ ↕ ➻ ➼ ➽ ➸ ➳ ➺ ➻ ➴ ➵ ➶ ➷ ➹".split("");
 
 ctx.font = "10px";
 
 function rain() {
     ctx.fillStyle = "rgba(0,0,20,0.05)";
-    ctx.fillRect(0,0,width,height);
+    ctx.fillRect(0, 0, width, height);
     ctx.fillStyle = '#00c8aa';
-    arr.forEach(function(value, index) {
-        ctx.fillText(str[Math.floor(Math.random() * str.length)],index * 10, value + 10);
+    // let i = 0;
+    arr.forEach(function (value, index) {
+        ctx.fillText(str[Math.floor(Math.random() * str.length)], index * 10, value + 10);
 
         arr[index] = value >= height || value > 8888 * Math.random() ? 0 : value + 10;
-        
+        // i = 8888 * Math.random();
+        // console.log(i);
+        // arr[index] = value > i ? 0 : value + 10;
+
     })
 }
 
